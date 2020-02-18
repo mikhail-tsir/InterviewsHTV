2ea4794091042a392aaabfef09c13c1479f48172
Mikhail Tsirlin <59180939+mikhail-tsir@users.noreply.github.com>
2020-02-18 15:42:15 -0500

    Update getAudio.py

--- getAudio.py
+++ getAudio.py
@@ -6,8 +6,8 @@ import boto3
 from botocore.client import Config
 import os
 import random
-ACCESS_KEY = 'AKIAIWMAC56IM4LRHWUA'
-SECRET_KEY = '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'
+ACCESS_KEY = 'AWS_ACCESS_KEY'
+SECRET_KEY = 'AWS_SECRET_KEY'
 bucketname = 'hack-the-valley-audio'
 #data = open(filename, 'rb')
 
