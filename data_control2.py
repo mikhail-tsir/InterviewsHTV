52e6c030924da3dfe63fbc47c33c6bd8ce94c752
Mikhail Tsirlin <59180939+mikhail-tsir@users.noreply.github.com>
2020-02-18 15:45:09 -0500

    Update data_control2.py

--- data_control2.py
+++ data_control2.py
@@ -5,7 +5,7 @@ from pymongo import MongoClient
 from pprint import pprint
 import random
 
-client = MongoClient("mongodb+srv://Wen:1234@cluster0-gvhmp.mongodb.net/test?retryWrites=true&w=majority")
+client = MongoClient("mongodb+srv://Wen:[PASSWORD]@cluster0-gvhmp.mongodb.net/test?retryWrites=true&w=majority")
 collectionUsers = client.interview.users
 collectionData = client.interview.storedData
 
